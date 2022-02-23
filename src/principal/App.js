2f5f0358ab381ef439b9df1a72eb313b291ffe4b
Daniel Xavier <dxavier194@gmail.com>
2022-02-23 10:50:43 -0300

    Form done

--- src/principal/App.js
+++ src/principal/App.js
@@ -1,5 +1,6 @@
 import AtividadeYup from './AtividadeYup';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../index.css';
 
 const App = () => <AtividadeYup />;
 
--- src/principal/AtividadeYup.js
+++ src/principal/AtividadeYup.js
@@ -4,17 +4,18 @@ import * as Yup from 'yup';
 import SelectInput from './SelectInput'
 
 const LoginSchema = Yup.object().shape({
-  nome: Yup.string().required('Required'),
-  idade: Yup.number().required('Required').min(17, 'Over 16!'),
-  cpf: Yup.string().required('Required').min(14, 'CPF must be size 14! (999.999.999-99)').max(14, 'CPF must be size 14! (999.999.999-99)').matches(/^[0-9\s\.\-]*$/,'CPF can only contain numbers, dash and dot. (999.999.999-99)'),
-  matricula: Yup.string().required('Required').min(9, 'Matrícula must be size 9!').max(9, 'Matrícula must be size 9!').matches(/^[0-9\s]*$/, 'Can only contain numbers.'), 
-  curso: Yup.string().required('Required'),
-  endereco: Yup.string().required('Required'),
+  nome: Yup.string().required('Obrigatório'),
+  idade: Yup.number().required('Obrigatório').min(17, 'Tem que ser maior de 16 anos!'),
+  cpf: Yup.string().required('Obrigatório').min(14, 'CPF deve ter tamanho 14! (000.000.000-00)').max(14, 'CPF deve ter tamanho 14! (000.000.000-00)').matches(/^[0-9\s\.\-]*$/,'CPF só pode conter números, traço e ponto. (000.000.000-00)'),
+  matricula: Yup.string().required('Obrigatório').min(9, 'Matrícula must be size 9!').max(9, 'Matrícula must be size 9!').matches(/^[0-9\s]*$/, 'Só pode conter números.'), 
+  curso: Yup.string().required('Obrigatório'),
+  endereco: Yup.string().required('Obrigatório'),
   numero: Yup.string(),
   complemento: Yup.string(),
-  bairro: Yup.string().required('Required'),
-  cidade: Yup.string().required('Required'),
-  cep: Yup.string().required('Required').min(9, 'CEP must be size 9! (00000-000)').max(9, 'CEP must be size 9! (00000-000)').matches(/^[0-9\s\-]*$/, 'Can only contain numbers and dash. (00000-000)')
+  bairro: Yup.string().required('Obrigatório'),
+  cidade: Yup.string().required('Obrigatório'),
+  estado: Yup.string().required('Obrigatório'),
+  cep: Yup.string().required('Obrigatório').min(9, 'CEP deve ter tamanho 9! (00000-000)').max(9, 'CEP deve ter tamanho 9! (00000-000)').matches(/^[0-9\s\-]*$/, 'CEP só pode conter números e traços. (00000-000)')
 });
 
 const FormYupValidation = () => {
@@ -58,6 +59,11 @@ const FormYupValidation = () => {
     { value: "brasilia", label: 'Brasilia'}
   ]  
 
+  const styleBody = {  
+    padding: '100px',
+    backgroundColor: 'rgb(188, 177, 207)'
+  };
+
   return (
     <Formik
       validationSchema={LoginSchema}
@@ -70,89 +76,89 @@ const FormYupValidation = () => {
           handleSubmit,
           isSubmitting,
         }) => (
-        <div className="container" align="center">
+        <div className="container" style={styleBody} >
         <form  onSubmit={handleSubmit} className="centered">
-          <label className="form-group">
+          <div className="form-group">
             Nome completo*:
             <Field className="form-control" type="text" name="nome"
                    onBlur={handleBlur}
                    onChange={handleChange}/>
-          </label>
+          </div>
           <ErrorMessage name="nome" className="error" component="span"/>
           <br/><br/>
-          <label  className="form-group">
+          <div  className="form-group">
             Idade*:
             <Field className="form-control" type="number" name="idade"
                    onBlur={handleBlur}
                    onChange={handleChange}/>
-          </label>
+          </div>
           <ErrorMessage name="idade" className="error" component="span" />
           <br/><br/>
-          <label  className="form-group">
+          <div  className="form-group">
             CPF*:
             <Field className="form-control" type="text" name="cpf"
                    onBlur={handleBlur}
                    onChange={handleChange}/>
-          </label>
+          </div>
           <ErrorMessage name="cpf" className="error" component="span"/>
           <br/><br/>
-          <label  className="form-group">
+          <div  className="form-group">
             Matrícula*:
             <Field className="form-control" type="text" name="matricula"
                    onBlur={handleBlur}
                    onChange={handleChange}/>
-          </label>
+          </div>
           <ErrorMessage name="matricula" className="error" component="span"/>
           <br/><br/>
-          <label  className="form-group">
+          <div  className="form-group">
             Curso*:
             <Field className="form-control" type="text" name="curso"
                    onBlur={handleBlur}
                    onChange={handleChange}/>
-          </label>
+          </div>
           <ErrorMessage name="curso" className="error" component="span"/>
           <br/><br/>
-          <label  className="form-group">
+          <div  className="form-group">
             Endereço*:
             <Field className="form-control" type="text" name="endereco"
                    onBlur={handleBlur}
                    onChange={handleChange}/>
-          </label>
+          </div>
           <ErrorMessage name="endereco" className="error" component="span"/>
           <br/><br/>
-          <label  className="form-group">
+          <div  className="form-group">
             Número:
             <Field className="form-control" type="text" name="numero"
                    onBlur={handleBlur}
                    onChange={handleChange}/>
-          </label>
+          </div>
           <ErrorMessage name="numero" className="error" component="span"/>
           <br/><br/>
-          <label  className="form-group">
+          <div  className="form-group">
             Complemento:
             <Field className="form-control" type="text" name="complemento"
                    onBlur={handleBlur}
                    onChange={handleChange}/>
-          </label>
+          </div>
           <ErrorMessage name="complemento" className="error" component="span"/>
           <br/><br/>
-          <label  className="form-group">
+          <div  className="form-group">
             Bairro*:
             <Field className="form-control" type="text" name="bairro"
                    onBlur={handleBlur}
                    onChange={handleChange}/>
-          </label>
+          </div>
           <ErrorMessage name="bairro" className="error" component="span"/>
           <br/><br/>
-          <label  className="form-group">
+          <div  className="form-group">
             Cidade*:
             <Field className="form-control" type="text" name="cidade"
                    onBlur={handleBlur}
                    onChange={handleChange}/>
-          </label>
+          </div>
           <ErrorMessage name="cidade" className="error" component="span"/>
           <br/><br/>
-          <label  className="form-group">
+          <div  className="form-group">
             Estado*:
             <SelectInput name="estado">
               {options.map((option) => (
@@ -161,18 +167,18 @@ const FormYupValidation = () => {
               </option>
               ))}
             </SelectInput>
-          </label>
+          </div>
           <ErrorMessage name="estado" className="error" component="span"/>
           <br/><br/>
-          <label  className="form-group">
+          <div  className="form-group">
             CEP*:
             <Field className="form-control" type="text" name="cep"
                    onBlur={handleBlur}
                    onChange={handleChange}/>
-          </label>
+          </div>
           <ErrorMessage name="cep" className="error" component="span"/>
           <br/><br/>
-          <input type="submit" value="Enviar" disabled={isSubmitting}/>
+          <input type="submit" value="Enviar" className="btn btn-primary"disabled={isSubmitting}/>
         </form>
         </div>
       )}
